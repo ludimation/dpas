25525ac193095f3b211aa2412df2e29a8e681349
NathanPalo <nathan.palo@gmail.com>
2014-03-05 13:44:53 -0500

    updated fire sounds
    not much new here, just a updated build for class

--- dpas_project/Assets/Scripts/Earth.cs
+++ dpas_project/Assets/Scripts/Earth.cs
@@ -18,22 +18,41 @@ public class Earth : MonoBehaviour {
 	int terrXBound;
 	int terrYBound;
 	int terrRes;
+	double terrXratio;
+	double terrYratio;
+
+	public Texture2D heightMap;
 	//int terrYRes;
 
 	public Terrain terr;
 	public Terrain terrOrig;
 	// Use this for initialization
 	void Start () {
+		terrXratio = terr.terrainData.heightmapWidth/(double)terr.terrainData.size.x;
+		terrYratio = terr.terrainData.heightmapHeight/(double)terr.terrainData.size.z;
+		Debug.Log (terrXratio.ToString ());
+
+		//heightMap.
+
+		terr.enabled = true;
+		terrOrig.enabled = false;
+		Debug.Log ("terr.enabled = "+ terr.enabled.ToString());
 		terrXBound = terr.terrainData.heightmapWidth;
 		terrYBound = terr.terrainData.heightmapHeight;
 		terrRes = terr.terrainData.detailResolution;
 		float[,] temp = terrOrig.terrainData.GetHeights (0,0, terrOrig.terrainData.heightmapWidth, terrOrig.terrainData.heightmapHeight);
 		terr.terrainData.SetHeights (0,0, temp);
-	
+		reset();
+		Debug.Log ("terr.enabled = "+ terr.enabled.ToString());
+
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKeyUp(KeyCode.L)){
+			reset();
+		}
 		if(!General.kinectControl){
 			/*lowEnergy += Time.deltaTime;
 			highEnergy += Time.deltaTime;*/
@@ -87,47 +106,56 @@ public class Earth : MonoBehaviour {
 		lowerIndicator.localScale = (1+lowEnergy)*Vector3.one;
 		upperIndicator.localScale = (1+highEnergy)*Vector3.one;
 	}
-	void deform (float radius = 50){
+	void deform (float radius = 100){
 		float x = transform.position.x-terr.transform.position.x;
 		float y = transform.position.z-terr.transform.position.z;
-		x /= terr.terrainData.size.x;
-		y /= terr.terrainData.size.z;
-		radius /= terr.terrainData.size.magnitude;
+		//x /= terr.terrainData.size.x;
+		//y /= terr.terrainData.size.z;
+		//radius /= terr.terrainData.size.magnitude;
 		//Debug.Log (x.ToString() +", "+ y.ToString ());
-		x *= terr.terrainData.heightmapWidth;
-		y *= terr.terrainData.heightmapHeight;
-		radius *= Mathf.Sqrt ((terr.terrainData.heightmapWidth*terr.terrainData.heightmapWidth) + (terr.terrainData.heightmapHeight*terr.terrainData.heightmapHeight));
+		//x *= terr.terrainData.heightmapWidth;
+		//y *= terr.terrainData.heightmapHeight;
+		//radius *= Mathf.Sqrt ((terr.terrainData.heightmapWidth*terr.terrainData.heightmapWidth) + (terr.terrainData.heightmapHeight*terr.terrainData.heightmapHeight));
 		//Debug.Log (x.ToString() +", "+ y.ToString ());
 
-		int tX = (int)x;
+		int tX = (int)(x*terrXratio);
 		//tX = Mathf.Max (tX, 0);
 		//tX = Mathf.Min (tX, terr.terrainData.heightmapWidth);
-		int tY = (int)y;
-		int tR = (int)radius;
+		int tY = (int)(y*terrYratio);
+		int tR = (int)(radius*Mathf.Max ((float)terrYratio, (float)terrXratio));
 		//tY = Mathf.Max (tY, 0);
 		//tY = Mathf.Min (tY, terr.terrainData.heightmapHeight);
 
 		//Debug.Log (tX.ToString() +", "+ tY.ToString ());
-		float[,] heights = terr.terrainData.GetHeights(tX-tR,tY-tR,tR*2,tR*2);
+		float[,] heights = terr.terrainData.GetHeights(tX-(int)(.5f*tR),tY-(int)(.5f*tR),tR*4,tR*4);
 		int width = heights.GetUpperBound (0);
 		int height = heights.GetUpperBound(1);
 		Vector2 temp = Vector2.zero;
 		Vector2 p =  tR*Vector2.one;
 		for(int i = 0; i<width; ++i){
 			for(int j = 0; j<height; ++j){
-				temp.x = i;
-				temp.y = j;
-				if(i == tR && j == tR){
-					heights[i,j] =.2f;
-				}
-				else{
-				heights[i,j] =.1f;
-				}
+				//temp.x = i;
+				//temp.y = j;
+				//if(i == tR-1 && j == tR-1){
+				//	heights[i,j] =0f;
+				//}
+				//else{
+				//	heights[i,j] = .1f+.001f*(i+j);
+				//}
+			
+				/*else{
+					heights[i,j] =.1f;
+				}*/
+				heights[i,j] = heights[i+1, j+1];
 
-				Debug.Log (temp.ToString ()+", "+p.ToString ()+": "+Vector2.Distance (temp,p).ToString());
+				//Debug.Log (temp.ToString ()+", "+p.ToString ()+": "+Vector2.Distance (temp,p).ToString());
 			}
 		}
 		terr.terrainData.SetHeights (tX-5,tY-5, heights);
+	}
+	void reset(){float[,] temp = terrOrig.terrainData.GetHeights (0,0, terrOrig.terrainData.heightmapWidth, terrOrig.terrainData.heightmapHeight);
+		terr.terrainData.SetHeights (0,0, temp);
+		//terr.enabled= true;
 	}
 
 	void OnGUI(){
