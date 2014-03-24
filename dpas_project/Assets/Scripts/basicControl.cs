3e53395196ada06b9b1c39e2a9d9514a45bf7943
NathanPalo <nathan.palo@gmail.com>
2014-03-23 23:09:01 -0400

    flying works way better
    flight and collisions at the same time.

--- dpas_project/Assets/Scripts/basicControl.cs
+++ dpas_project/Assets/Scripts/basicControl.cs
@@ -3,24 +3,17 @@ using System.Collections;
 
 public class basicControl : MonoBehaviour {
 	public Kinectalogue inPut;
-	//public float sensetivity = 10;
 	public float posCutoff = 1;
 	public Vector3 sensetivity = Vector3.one;
 	public Vector3 rotSensetivity = Vector3.up;
-	//public float pitchSensetivity = 1;
-	//public float yawSensetivity = 1;
-	//public float rollSensetivity = 1;
-	//public float floor = 0;
-	//public int mode = 0;
 
 	public float deadzone = .01f;
 	public Vector3 rotDeadzone = 5*Vector3.one;
 	public CharacterController cha;
-	public CharacterMotor motor; 
+	public CharacterMotor motor;
+	public bool fly = false;
 	// Use this for initialization
 	void Start () {
-		//motor = (CharacterMotor)gameObject.GetComponent (CharacterMotor);
-		//motor = gameObject.GetComponent<CharacterMotor>();
 		motor = cha.GetComponent<CharacterMotor>();
 	}
 	
@@ -71,18 +64,20 @@ public class basicControl : MonoBehaviour {
 			rot.z -= Mathf.Sign (rot.z)*rotDeadzone.z;
 		}
 		if(!General.kinectControl){
-			//transform.Translate (sensetivity * Time.deltaTime * new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis ("Flight"), Input.GetAxis ("Vertical")));
-			//transform.Rotate (0, Input.GetAxis ("Mouse X")*yawSensetivity, 0);
 			move += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis ("Flight"), Input.GetAxis ("Vertical"));
 			rot += 45*new Vector3(0, Input.GetAxis ("Yaw"), 0);
 		}
-		//rot.Scale (new Vector3(Mathf.Abs (rot.x)/rotSensetivity.x, Mathf.Abs (rot.y)/rotSensetivity.y, Mathf.Abs (rot.z)/rotSensetivity.z));
 		rot.Scale (new Vector3(Mathf.Abs (rot.x), Mathf.Abs (rot.y), Mathf.Abs (rot.z)));
 		rot.Scale ((1f/90f)*Vector3.one);
 		move.Scale (Time.deltaTime*sensetivity);
 		rot.Scale (Time.deltaTime*rotSensetivity);
-		motor.inputMoveDirection = transform.rotation*move;
-		//transform.Translate(move);
+		if(fly){
+			//transform.Translate (move);
+			cha.Move (transform.rotation*move);
+		}
+		else{
+			motor.inputMoveDirection = transform.rotation*Vector3.Scale(move, new Vector3(1,0,1));
+		}
 		transform.Rotate (rot);
 
 
