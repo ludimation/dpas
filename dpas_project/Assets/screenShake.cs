33e7e3176e14aa4c4850b15bd5c569e960b3cccc
david allen <ludimation@glacier-20.dynamic.rpi.edu>
2014-03-31 15:02:08 -0400

    screenShake stub code FOR REALZIES this time.

--- dpas_project/Assets/screenShake.cs
+++ dpas_project/Assets/screenShake.cs
@@ -7,11 +7,13 @@ using System.Collections;
 
 // Should know how to create default shake effects for all FX types
 
-// Should queue all effects so that they can be applied in one step
-// - charge shakes (should really only be one at any given point, but create a list just in case
+// Should queue all effects into arrays so that they can be applied in one step
+// - charge shakes (should really only be one at any given point, but create an array just in case)
 // - impact shakes
 
 enum slope {
+	SLOPE_LIN_ON,			// x
+	SLOPE_LIN_OFF,			// -x + 1
 	SLOPE_SQ_EASEOUT_ON, 	// x^2
 	SLOPE_SQ_EASEOUT_OFF, 	// x^2 + 1
 	SLOPE_SQ_EASEIN_ON,		// -(x-1)^2 + 1
@@ -43,34 +45,52 @@ enum impactType {
 	IMPACT_COUNT
 }
 
+// TODO: Figure out a way to set defaults in constructor?
+
 public struct charge {
-	public string 	type 		: chargeType.CHARGE_LIGHTNING;
-	public float 	charge 		: 0.5f; // 0.0f – 1.0f
-	public float 	amplitude 	: 1.0f; // any value (should be relatively lower for charge FX compared to Impact FX?)
-	public float 	slope 		: slope.SLOPE_SQRTIN; // might be a naming conflict
-	public float 	smoothing	: 0.1f;
+	public string 	type 		;// : chargeType.CHARGE_LIGHTNING;
+	public float 	amount 		;// : 0.5f; // 0.0f – 1.0f
+	public float 	amplitude 	;// : 1.0f; // any value (should be relatively lower for charge FX compared to Impact FX?)
+	public float 	slope 		;// : slope.SLOPE_SQRTIN; // might be a naming conflict
+	public float 	smoothing	;// : 0.1f;
+	public float 	duration	;// : 1000.0f; // seconds (might want to put this in here anyway so they self-destruct after a while if forgotten
 }
 
 public struct impact {
-	public string 	type 		: impactType.IMPACT_LIGHTNING;
-	public float 	amplitude 	: 1.0f; // any value (should be relatively lower for charge FX compared to Impact FX?)
-	public float 	slope		: slop.SLOPE_SQRT; // might be a naming conflict
-	public float 	duration	: 0.1f;
+	public string 	type 		;// : impactType.IMPACT_LIGHTNING;
+	public float 	amplitude 	;// : 1.0f; // any value (should be relatively lower for charge FX compared to Impact FX?)
+	public float 	slope		;// : slop.SLOPE_SQRT; // might be a naming conflict
+	public float 	duration	;// : 0.1f;
 }
 
 public class screenShake : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		// initialize properties
+		// - array of charge structs
+		// - array of impact structs
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// cycle through impact and charge structs to calculate screen shake
+		// use delta time for duration-specific ones
+		// reset position of camera if none exist
 	}
 
+	void NewImpact () {
+		// create new struct with arguments handed in (or defaults)
+		// add it to the array of impact structs
+	}
+	
 	void NewCharge () {
-		// 
+		// create new struct with arguments handed in (or defaults)
+		// add it to the array of chage structs
+		// return a unique ID for updates to this charge effect
+	}
+
+	void UpdateCharge () {
+		// should change the value (0.0f to 1.0f) of a particular charge with the specified ID
 	}
 }
