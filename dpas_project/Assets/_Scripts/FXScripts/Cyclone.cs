8529d732b5a90826293924d5723a20539b9c37ca
kevan1747 <kevan1747@gmail.com>
2014-04-02 01:36:32 -0400

    Lots of air and cloud stuff

--- dpas_project/Assets/_Scripts/FXScripts/Cyclone.cs
+++ dpas_project/Assets/_Scripts/FXScripts/Cyclone.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class Cyclone : MonoBehaviour {
-	public Vector3 _point = Vector3.zero;
+	public Transform t;
+	Vector3 _point = Vector3.zero;
 	void Start () {
 		//_point = new Vector3(1,1,1)
 	}
 	// Update is called once per frame
 	void Update () {
+		_point = t.position;
 		//this.transform.Rotate (Vector3.right, Time.deltaTime * (100.0f), Space.World);
 		transform.RotateAround (_point, Vector3.up, 200 * Time.deltaTime);
 	
