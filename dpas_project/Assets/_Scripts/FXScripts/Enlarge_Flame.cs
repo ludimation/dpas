4f317146b3fe7f82a877b2c04d245142501f845f
kevan1747 <kevan1747@gmail.com>
2014-04-21 00:09:42 -0400

    progress on flamethrower
    right now 'best_flamethrower" is the one that works the best, but
    I'm trying to make a cooler looking one

---
+++ dpas_project/Assets/_Scripts/FXScripts/Enlarge_Flame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enlarge_Flame : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += new Vector3(1* Time.deltaTime, 1* Time.deltaTime, 1* Time.deltaTime);
+	
+	}
+}
